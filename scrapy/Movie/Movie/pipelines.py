a18dfad5e6ccafc60396c2ee98b92212b907de4c
time-river <feather12315@live.com>
2016-04-01 23:08:20 +0800

    update

--- scrapy/Movie/Movie/pipelines.py
+++ scrapy/Movie/Movie/pipelines.py
@@ -7,6 +7,66 @@
 
 import pickle
 import redis
+from Movie.items import MovieInformation
+
+def list_ss(obj, *, ch=None, ar=None):
+    '''
+    list split and strip
+    '''
+    objects = list()
+    if not isinstance(obj ,list):
+        obj = [obj]
+    if ar:
+        for item in obj:
+            objects.extend(item.split(ar))
+    else:
+        objects = obj
+    results = list(map(lambda x: x.strip(ch), objects))
+    return results 
+
+def information_filter(item):
+    # id movie director writer actor type official_site release_data
+    info = MovieInformation()
+    if item['id']:
+        info['id'] = list_ss(item['id'])
+    if item['year']:
+        info['year'] = list_ss(item['year'], ch='() ')
+    if item['movie']:
+        info['movie'] = list_ss(item['movie'])
+    if item['director']:
+        info['director'] = list_ss(item['director'])
+    if item['writer']:
+        info['writer'] = list_ss(item['writer'])
+    if item['actor']:
+        info['actor'] = list_ss(item['actor'])
+    if item['type']:
+        info['type'] = list_ss(item['type'])
+    if item['official_site']:
+        info['official_site'] = list_ss(item['official_site'])
+    if item['area']:
+        info['area'] = list_ss(item['area'], ar='/')
+    if item['language']:
+        info['language'] = list_ss(item['language'], ar='/')
+    if item['release_data']:
+        info['release_data'] = list_ss(item['release_data'])
+    if item['season']:
+        info['season'] = list_ss(item['season'])
+    if item['episodes']:
+        info['episodes'] = list_ss(['episodes'])
+    if item['single_runtime']:
+        info['single_runtime'] = list_ss(item['single_runtime'])
+    if item['alias']:
+        info['alias'] = list_ss(item['alias'], ar='/')
+    if item['imdb']:
+        info['imdb'] = list_ss(item['imdb'])
+    if item['synopsis']:
+        info['synopsis'] = list_ss(item['synopsis'])
+    if item['awards']:
+        def award_filter(obj):
+            obj['year'] = list_ss(obj['year'], ch=' ()\xa0')
+            return obj
+        info['awards'] = list(map(award_filter, item['awards']))
+    return info
 
 class MoviePipeline(object):
     def __init__(self):
@@ -15,24 +75,5 @@ class MoviePipeline(object):
     def process_item(self, item, spider):
         item = information_filter(item)
         data = dict(item)
-        self.redis.rpush('test.movie.information', pickle.dumps(data))
-        return item
-
-    @staticmethod
-    def information_filter(item):
-        if item['year']:
-            item['year'] = item['year'][0].strip('() ')
-        if item['area']:
-            item['area'] = item['area'][0].replace('/', ' ').split()
-        if item['language']:
-            item['language'] = item['language'][0].replace('/', ' ').split()
-        if item['alias']:
-            item['alias'] = item['alias'][0].replace('/', ' ').split()
-        if item['synopsis']:
-            item['synopsis'] = list(map(lambda string: string.strip(), item['synopsis']))
-        if item['awards']:
-            def award_filter(obj):
-                obj['year'] = obj['year'][0].strip('() ')
-                return obj
-            item['awards'] = list(map(award_filter, item['awards']))
+        self.redis.rpush('test', pickle.dumps(data))
         return item
--- scrapy/Movie/Movie/spiders/movie.py
+++ scrapy/Movie/Movie/spiders/movie.py
@@ -14,7 +14,6 @@ class MovieSpider(Spider):
     ]
 
     def parse(self, response):
-#    def test(self, response):
         selector = Selector(response)
         urls = selector.xpath('//table[last()]/tbody/tr/td/a/@href').extract()
         for raw_url in map(lambda url: url.replace('www', 'movie'), urls):
@@ -22,7 +21,6 @@ class MovieSpider(Spider):
             yield Request(url=url, callback=self.movieParse)
             
     def movieParse(self, response):
-    #def parse(self, response):
         selector = Selector(response)
         movie_urls = selector.xpath('//td[@valign="top"]/div/a/@href').extract()
         for url in movie_urls:
@@ -48,7 +46,12 @@ class MovieSpider(Spider):
         information['language'] = selector.re(r'<span[\s]+class="pl">语言[\W]+</span>(.*?)<br')
         information['release_data'] = selector.xpath('//*[@id="info"]/span[@property="v:initialReleaseDate"]/@content').extract()
         # 连续剧内容 
-        information['season'] = selector.xpath('//*[@id="season"]/option[@selected="selected"]/text()').extract()
+        season_1 = selector.xpath('//*[@id="season"]/option[@selected="selected"]/text()').extract()
+        seasion_2 = selector.re(r'<span[\s]+class="pl">季数[\W]+</span>(.*?)<br')
+        if season_1: # 两种模式
+            information['season'] = seasion_1
+        else:
+            information['season'] = seasion_2
         information['episodes'] = selector.re(r'<span[\s]+class="pl">集数[\W]+</span>(.*?)<br')
         information['single_runtime'] = selector.re(r'<span[\s]+class="pl">单集片长[\W]+</span>(.*?)<br')
         # end
@@ -56,6 +59,7 @@ class MovieSpider(Spider):
         information['alias'] = selector.re(r'<span[\s]+class="pl">又名[\W]+</span>(.*?)<br')
         information['imdb'] = selector.xpath('//*[@id="info"]/a[@target="_blank" and @rel="nofollow"]').re(r'href="([\S]+\.\bimdb\b\.[\S]+)"') # re(r'href="([\S]+\.imdb\.[\S]+)"') re(r'href="([\S]+imdb[\S]+)"')
         information['synopsis'] = selector.xpath('//*[@id="link-report"]//span[@property="v:summary"]/text()').extract() # 相对定位，解决<span>嵌套问题
+        information['awards'] = list()
         awards_url = selector.css('#content > div > div.article > div.mod > div > h2 > span > a::attr(href)').extract()
         if awards_url:
             yield Request(url=awards_url[0], meta={'info': information}, callback=self.awardsParse)
@@ -64,7 +68,6 @@ class MovieSpider(Spider):
 
     def awardsParse(self, response):
         information = response.meta['info']
-        information['awards'] = list()
         selector = Selector(response)
         items = selector.css('#content > div > div.article > div.awards')
         for item in items:
